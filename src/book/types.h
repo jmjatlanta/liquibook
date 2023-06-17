1c1f2419359e5bab229781dca74d58f55032ec12
John Jones <jmjatlanta@gmail.com>
2023-06-17 13:28:43 -0500

    adjust types

--- src/book/types.h
+++ src/book/types.h
@@ -9,9 +9,9 @@
 
 namespace liquibook { namespace book {
   // Types used in Liquibook
-  typedef uint64_t Price;
+  typedef int64_t Price;
   typedef uint64_t Quantity;
-  typedef uint64_t Cost;
+  typedef int64_t Cost;
   typedef uint32_t FillId;
   typedef uint32_t ChangeId;
   typedef uint32_t OrderConditions;
